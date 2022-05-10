e2dc378c0012da76a0d58e9912e5ad3c336bce6d
Torikul <toriqulislam142@gmail.com>
2022-05-10 00:45:47 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/appointment' element={<Appointment />} />
         <Route path='/review' element={<Review />} />
