1cd11ebac62ac8879453fba506ef09b78208690c
Torikul <toriqulislam142@gmail.com>
2022-05-10 00:21:38 -0500

    project ready for work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Appointment from './pages/Appointment/Appointment';
+import ContactUs from './pages/ContactUs/ContactUs';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Navbar from './pages/Navbar/Navbar';
+import Review from './pages/Review/Review';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/appointment' element={<Appointment />} />
+        <Route path='/review' element={<Review />} />
+        <Route path='/contactUs' element={<ContactUs />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+
     </div>
   );
 }
